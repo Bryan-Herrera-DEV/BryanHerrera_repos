697332b046cfb3d2803bc20c5482c91979a6b972
Bryan-Herrera-DEV <hbryanelias@gmail.com>
2022-07-07 21:27:17 -0500

    (feat) test: test a rutas

--- tests/prueba.test.ts
+++ tests/prueba.test.ts
@@ -11,3 +11,19 @@ describe('GET Test: /organizations ', () => {
 		expect(response.statusCode).toBe(200);
 	});
 });
+
+describe('Escenario 1: Obtener métricas de repositorios por tribu:', () => {
+  it('Debe devolver un código de estado 200', async () => {
+    const response = await request(baseUrl)
+      .get('repository/1');
+    expect(response.statusCode).toBe(200);
+  });
+})
+
+describe('Escenario 2: Tribu inexistente:', () => {
+  it('Debe devolver un código de estado 404', async () => {
+    const response = await request(baseUrl)
+      .get('repository/2');
+    expect(response.statusCode).toBe(404);
+  });
+})
